35c41869495863ed027d3a62dd23ca188d877a22
uhispano1 <patrick.robinson0265@uhispano.ac.cr>
2024-12-20 20:46:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ st.write(
 
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app
 # reruns (e.g. if the user interacts with the widgets).
-@st.cache_data
+# @st.cache_data
 def load_data():
     df = pd.read_csv("data/notas.csv")
     return df
